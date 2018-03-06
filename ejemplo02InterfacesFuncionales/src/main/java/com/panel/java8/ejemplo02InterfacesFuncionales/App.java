f4d67ffcb51c2e55502463acc95c5f350d4f382e
Rubén Gómez García <rgomez@pronoide.es>
2018-03-06 18:45:30 +0000

    Ejemplos interfaz funcional y arraylist tipado-sin tipar

---
+++ ejemplo02InterfacesFuncionales/src/main/java/com/panel/java8/ejemplo02InterfacesFuncionales/App.java
@@ -0,0 +1,78 @@
+package com.panel.java8.ejemplo02InterfacesFuncionales;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+@FunctionalInterface
+interface VerPartido {
+	void campo(String nombreDelCampo);
+}
+class AhoraVieneLoBueno{
+	public void hazLoQueQuieras(String cadena,Consumer<String> consumidor) {
+		consumidor.accept(cadena);
+	}
+}
+class ConClase {
+	
+	public ConClase(String valor) {
+		super();
+		System.out.println("Acabo de crearme con valor " + valor + "!");
+	}
+	public ConClase() {
+		super();
+	}
+	void estoEsUnMetodo(String cadena) {
+		System.out.println("Yo visto " + cadena + " con clase");
+	}
+}
+interface ConClaseYObject <T>{
+		T estoDevuelveUnObject(String loQueNecesite);
+}
+
+public class App {
+	public static void main(String[] args) {
+		VerPartido tele = null;
+
+		tele = System.out::println;
+		tele.campo("Parque de los Principes");
+		ConClase objetoConClase = new ConClase();
+		tele = objetoConClase::estoEsUnMetodo;
+		tele = new ConClase()::estoEsUnMetodo;
+		//tele = new ConClase()::estoEsUnMetodo;
+
+		tele.campo("Parque de los Principes");
+		ConClaseYObject<ConClase> ccyo = ConClase::new;
+		ConClase objeto = ccyo.estoDevuelveUnObject("Mis miserias");
+		objeto.estoEsUnMetodo("y sus cosas");
+		AhoraVieneLoBueno esoEs = new AhoraVieneLoBueno();
+		esoEs.hazLoQueQuieras("Raaro raaro", System.out::println);
+		esoEs.hazLoQueQuieras("otra cadena", new ConClase()::estoEsUnMetodo);
+		
+		
+		List<ConClase> lista = new ArrayList<>();
+		lista.add(objeto);
+		algo(lista);
+		lista.size();
+		// esto nooooo!!!! lista.add("asldijasd");
+		
+		// consumer: public void accept(T t)
+		Consumer<String> consumidor = txt -> System.out.println("valor  : " + txt);
+		consumidor.accept("valor");
+		
+		// predicate: public boolean test(T t)
+		Predicate<String> filtro = txt -> txt.length() < 10;
+		System.out.println("valor: "+filtro.test("fistror"));
+		System.out.println("valor: "+filtro.test("Fistro pecador de la pradera de la muerte mala"));
+		
+		
+		
+		
+		
+		
+	}
+	public static void algo(List lista) {
+		lista.add("li que me de la gana");
+	}
+}
